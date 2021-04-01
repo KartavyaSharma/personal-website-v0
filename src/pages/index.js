c8d75342c9ce70b8e13fdac43db70322d6df598e
KartavyaSharma <Kartavya@outlook.com>
2021-04-01 07:16:15 +0100

    Fixed Vercel deployment error path

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Navbar from "../components/navbar"
 import Intro from "../components/intro"
 
 const mobileHeaders = "py-12 md:py-16 lg:py-24 xl:py-32 2xl:py-48 px-6 md:px-8 lg:px-12 xl:px-20 2xl:px-36 max-w-screen-2xl w-full mx-auto font-mono";
