b297da78b6e11ba4914f59d7f8fdc84c6470fd52
KartavyaSharma <Kartavya@outlook.com>
2021-04-01 11:39:12 +0100

    Added profile image + updated body

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Myself from '../images/myself.jpg'
+
 function aboutme(props) {
     return (
         <div className={props.header} id="about-me">
@@ -7,8 +9,11 @@ function aboutme(props) {
                 <div className={props.topNum}>01</div>
                 <div className={props.topTitle}>About me</div>
             </div>
-            <div className={`flex flex-row pl-11 ${props.body}`}>
-                Hey, this is about me!
+            <div className='flex flex-row'>
+                <div className={`pl-11 ${props.body}`}>
+                    Hullo.
+                </div>
+                <img className='h-64 w-64 rounded-full my-4' src={Myself} alt='' />
             </div>
         </div>
     )
