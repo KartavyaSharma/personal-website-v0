1e71c05e21b5e2c1fb9e646e9dda9edfed7b6c6d
KartavyaSharma <Kartavya@outlook.com>
2021-04-04 09:55:20 +0100

    New branch init [adding-blog]

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
+import BlogList from "../components/BlogList"
+
 function Blog(props) {
     return (
         <div className={props.header} id="blog">
             <div className={props.topNum}>03</div>
             <div className={props.topTitle}>My blog</div>
             <div className={props.body}>
-                
+                <BlogList />
             </div>
         </div>
     )
