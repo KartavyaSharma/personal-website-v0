a4a191c8fdb106d39fdc8f60e580b5668ff28780
KartavyaSharma <Kartavya@outlook.com>
2021-04-09 05:58:38 +0100

    Revised projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,23 +5,14 @@ import Myself from "../images/Salkatech.png"
 import Roze from "../images/ROZE.png"
 import Prime from "../images/Prime_logo.png"
 
-// const cardAnimation = "transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110 ";
-const cardAnimation = "";
-const cardElemnentStyle = "max-w-md mr-auto border border-gray-500 hover:border-orange-500 hover:bg-orange-500 hover:bg-opacity-5 rounded-xl shadow-md overflow-hidden md:max-w-2xl mt-4 ";
-const cardBodyPaddingLeft = "pl-4 pr-8 pt-8 pb-4 ";
-const cardBodyPaddingRight = "xl:pr-4 xl:pl-8 xl:pt-8 xl:pb-4 ";
-const cardBodyCategory = "uppercase tracking-wide text-sm text-orange-500 font-semibold ";
-const cardBodyTitle = "block mt-1 text-lg leading-tight font-medium text-white hover:underline ";
-const cardBodyText = "mt-2 text-gray-400 ";
-
 function Projects(props) {
     return (
         <div className={props.header} id="projects">
             <div className={props.topNum}>02</div>
             <div className={props.topTitle}>Projects</div>
-            <div className="h-auto pt-14 pb-10 overflow-hidden flex items-center justify-center" style={{background: '#edf2f7'}}>
+            <div className="h-full mt-14 mb-10 overflow-hidden flex items-center justify-center" style={{ background: '#edf2f7' }}>
                 <section className="text-indigo-200 body-font p-5 bg-trueGray-800 pb-10">
-                    <Link to="coursedet">
+                    <div>
                         <div className="mx-auto flex px-5  md:flex-row flex-col items-center jobcard">
                             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center">
 
@@ -36,13 +27,27 @@ function Projects(props) {
                                             <p className="text-indigo-200 text-base pb-8">We can't believe how far we have come in the last 6 months. I really did not think this awesome career move would come so quickly. Thanks to each of you put into SI and the partner relationships.</p>
                                             <div className="flex items-center justify-between">
                                                 <div className="flex items-center pb-12">
-                                                    <div className="h-12 w-12">
+                                                    {/* <div className="h-12 w-12">
                                                         <img src="https://tuk-cdn.s3.amazonaws.com/assets/components/testimonials/t_1.png" alt className="h-full w-full object-cover overflow-hidden rounded-full" />
                                                     </div>
                                                     <p className="text-indigo-200 font-bold ml-3">
                                                         Jane Doe <br />
                                                         <span className="text-indigo-200 text-base font-light">Apple Inc</span>
-                                                    </p>
+                                                    </p> */}
+                                                    <Link to='https://salkatech.com'>
+                                                        <button className='py-2 px-4 mr-4 text-white rounded-lg border border-white hover:border-orange-500 hover:bg-orange-500 flex'>
+                                                            Visit
+                                                            <svg className='ml-2 mt-0.5' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                                                                <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z" />
+                                                                <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z" />
+                                                            </svg>
+                                                        </button>
+                                                    </Link>
+                                                    <button className='py-2 px-4 text-white rounded-lg border border-white hover:border-orange-500 hover:bg-orange-500 flex'>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                                                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                                                        </svg>
+                                                    </button>
                                                 </div>
 
                                             </div>
@@ -55,85 +60,82 @@ function Projects(props) {
                                 <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
                             </div>
                         </div>
-                    </Link>
+                    </div>
                 </section>
             </div>
-            <div className="h-full mb-10 overflow-hidden flex items-center justify-center" style={{background: '#edf2f7'}}>
+            <div className="h-full mb-10 overflow-hidden flex items-center justify-center" style={{ background: '#edf2f7' }}>
                 <section className="text-indigo-200 body-font p-5 bg-trueGray-800 pb-10">
-                    <Link to="coursedet">
-                        <div className="mx-auto flex px-5  md:flex-row flex-col items-center jobcard">
-                            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center">
+                    <div className="mx-auto flex px-5  md:flex-row flex-col items-center jobcard">
+                        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center">
 
-                                <figure className="visible">
+                            <figure className="visible">
 
-                                    <div className="">
+                                <div className="">
 
-                                        <div className="pt-10 px-2 sm:px-6">
-                                            <span className="inline-block py-1 px-2 rounded-full bg-orange-500 text-white  text-xs font-bold tracking-widest mb-2">Contract</span>
-                                            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-100">Before they sold out <br className="hidden lg:inline-block" />readymade gluten</h1>
-                                            <p className="text-indigo-200 text-base pb-6">From local banks to local government, we partner with organizations on their journey to digital transformation. Our customers include 15 million professionals in 175 countries and 800 of the fortune 1000.</p>
-                                            <p className="text-indigo-200 text-base pb-8">We can't believe how far we have come in the last 6 months. I really did not think this awesome career move would come so quickly. Thanks to each of you put into SI and the partner relationships.</p>
-                                            <div className="flex items-center justify-between">
-                                                <div className="flex items-center pb-12">
-                                                    <div className="h-12 w-12">
-                                                        <img src="https://tuk-cdn.s3.amazonaws.com/assets/components/testimonials/t_1.png" alt className="h-full w-full object-cover overflow-hidden rounded-full" />
-                                                    </div>
-                                                    <p className="text-indigo-200 font-bold ml-3">
-                                                        Jane Doe <br />
-                                                        <span className="text-indigo-200 text-base font-light">Apple Inc</span>
-                                                    </p>
-                                                </div>
-
+                                    <div className="pt-10 px-2 sm:px-6">
+                                        <span className="inline-block py-1 px-2 rounded-full bg-orange-500 text-white  text-xs font-bold tracking-widest mb-2">Project</span>
+                                        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-100">Before they sold out <br className="hidden lg:inline-block" />readymade gluten</h1>
+                                        <p className="text-indigo-200 text-base pb-6">From local banks to local government, we partner with organizations on their journey to digital transformation. Our customers include 15 million professionals in 175 countries and 800 of the fortune 1000.</p>
+                                        <p className="text-indigo-200 text-base pb-8">We can't believe how far we have come in the last 6 months. I really did not think this awesome career move would come so quickly. Thanks to each of you put into SI and the partner relationships.</p>
+                                        <div className="flex items-center justify-between">
+                                            <div className="flex items-center pb-12">
+                                                <button className='py-2 px-4 text-white rounded-lg border border-white hover:border-orange-500 hover:bg-orange-500 flex'>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                                                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                                                    </svg>
+                                                </button>
                                             </div>
+
                                         </div>
                                     </div>
-                                </figure>
+                                </div>
+                            </figure>
 
-                            </div>
-                            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 sm:block hidden">
-                                <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
-                            </div>
                         </div>
-                    </Link>
+                        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 sm:block hidden">
+                            <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+                        </div>
+                    </div>
                 </section>
             </div>
-            <div className="h-full overflow-hidden flex items-center justify-center" style={{background: '#edf2f7'}}>
+            <div className="h-full overflow-hidden flex items-center justify-center" style={{ background: '#edf2f7' }}>
                 <section className="text-indigo-200 body-font p-5 bg-trueGray-800 pb-10">
-                    <Link to="coursedet">
-                        <div className="mx-auto flex px-5  md:flex-row flex-col items-center jobcard">
-                            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center">
+                    <div className="mx-auto flex px-5  md:flex-row flex-col items-center jobcard">
+                        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center">
 
-                                <figure className="visible">
+                            <figure className="visible">
 
-                                    <div className="">
+                                <div className="">
 
-                                        <div className="pt-10 px-2 sm:px-6">
-                                            <span className="inline-block py-1 px-2 rounded-full bg-orange-500 text-white  text-xs font-bold tracking-widest mb-2">Contract</span>
-                                            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-100">Before they sold out <br className="hidden lg:inline-block" />readymade gluten</h1>
-                                            <p className="text-indigo-200 text-base pb-6">From local banks to local government, we partner with organizations on their journey to digital transformation. Our customers include 15 million professionals in 175 countries and 800 of the fortune 1000.</p>
-                                            <p className="text-indigo-200 text-base pb-8">We can't believe how far we have come in the last 6 months. I really did not think this awesome career move would come so quickly. Thanks to each of you put into SI and the partner relationships.</p>
-                                            <div className="flex items-center justify-between">
-                                                <div className="flex items-center pb-12">
-                                                    <div className="h-12 w-12">
-                                                        <img src="https://tuk-cdn.s3.amazonaws.com/assets/components/testimonials/t_1.png" alt className="h-full w-full object-cover overflow-hidden rounded-full" />
-                                                    </div>
-                                                    <p className="text-indigo-200 font-bold ml-3">
-                                                        Jane Doe <br />
-                                                        <span className="text-indigo-200 text-base font-light">Apple Inc</span>
-                                                    </p>
-                                                </div>
-
+                                    <div className="pt-10 px-2 sm:px-6">
+                                        <span className="inline-block py-1 px-2 rounded-full bg-orange-500 text-white  text-xs font-bold tracking-widest mb-2">Project</span>
+                                        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-100">Before they sold out <br className="hidden lg:inline-block" />readymade gluten</h1>
+                                        <p className="text-indigo-200 text-base pb-6">From local banks to local government, we partner with organizations on their journey to digital transformation. Our customers include 15 million professionals in 175 countries and 800 of the fortune 1000.</p>
+                                        <p className="text-indigo-200 text-base pb-8">We can't believe how far we have come in the last 6 months. I really did not think this awesome career move would come so quickly. Thanks to each of you put into SI and the partner relationships.</p>
+                                        <div className="flex items-center justify-between">
+                                            <div className="flex items-center pb-12">
+                                            <Link to=''>
+                                                    <button className='py-2 px-4 mr-4 text-white rounded-lg border border-white hover:border-orange-500 hover:bg-orange-500 flex'>
+                                                        In dev
+                                                    </button>
+                                                </Link>
+                                                <button className='py-2 px-4 text-white rounded-lg border border-white hover:border-orange-500 hover:bg-orange-500 flex'>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                                                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                                                    </svg>
+                                                </button>
                                             </div>
+
                                         </div>
                                     </div>
-                                </figure>
+                                </div>
+                            </figure>
 
-                            </div>
-                            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 sm:block hidden">
-                                <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
-                            </div>
                         </div>
-                    </Link>
+                        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 sm:block hidden">
+                            <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+                        </div>
+                    </div>
                 </section>
             </div>
         </div>
