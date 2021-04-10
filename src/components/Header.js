e51f618444d6026c76971a1cbfb70909e60aa109
KartavyaSharma <Kartavya@outlook.com>
2021-04-10 09:21:16 +0100

    Added Header.js link

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,16 @@ function Header() {
     return (
         <div className="py-8 md:py-12 lg:py-16 lg:pb-48 px-6 md:px-8 lg:px-12 xl:px-20 2xl:px-36 max-w-screen-2xl w-full mx-auto font-mono flex flex-row">
             <div>
-                <div className='text-xl sm:text-2xl md:text-3xl font-extrabold'>
-                    <span className='text-orange-500'>
-                        Hello there! I'm
-                    </span>
-                </div>
-                <div className="text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-extrabold">
-                    Kartavya Sharma<span className="bg-clip-text text-orange-500">.</span>
-                </div>
+                <Link to="/">
+                    <div className='text-xl sm:text-2xl md:text-3xl font-extrabold'>
+                        <span className='text-orange-500'>
+                            Hello there! I'm
+                        </span>
+                    </div>
+                    <div className="text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-extrabold">
+                        Kartavya Sharma<span className="bg-clip-text text-orange-500">.</span>
+                    </div>
+                </Link>
             </div>
             <div className='mx-auto mr-1'>
                 <nav className='justify-between items-center relative font-semibold text-xl lg:text-2xl py-0 xl:py-4 text-orange-500'>
