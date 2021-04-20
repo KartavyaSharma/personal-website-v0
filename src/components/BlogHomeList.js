d7b8021ed02dfc0e92eb7e433ea39bd6a40826cd
KartavyaSharma <Kartavya@outlook.com>
2021-04-20 11:34:43 +0100

    fix: grid layout for blog page is now functional

--- src/components/BlogHomeList.js
+++ src/components/BlogHomeList.js
@@ -6,46 +6,29 @@ function BlogHomeList(props) {
 
     function renderBlogPostList() {
         return (
-            <div>
+            <div className='grid grid-rows-3 justify-center items-center gap-y-4'>
                 {
                     props.listData.filter(post => post.node.frontmatter.title !== "")
                     .map(post => {
                         const img = getImage(post.node.frontmatter.thumbnail);
                         return (
-                            <div className='max-w-4xl flex flex-col justify-center'>
-                                <div className='grid sm:grid-cols-4 lg:grid-cols-3'>
-                                    <div className='pt-7 md:pt-10 lg:pt-14 sm:col-span-3 lg:col-span-2'>
-                                        <Link to={post.node.fields.slug} key={post.node.id} >
+                            <div className='grid grid-cols-3 items-center'>
+                                    <div className='justify-start items-center gap-y-4 col-span-2'>
+                                        <Link to={post.node.fields.slug} key={post.node.id}>
                                             <div className='text-2xl md:text-3xl font-semibold hover:text-orange-500 hover:cursor-pointer hover:no-underline
-                                            hover:transition hover:ease-in-out transform hover:translate-x-4 duration-300'>{post.node.frontmatter.title}</div>
+                                            hover:transition hover:ease-in-out transform hover:translate-x-4 duration-300 max'>{post.node.frontmatter.title}</div>
                                         </Link>
-                                        {/* <div className='text-base md:text-xl text-gray-400 italic'>{blog.node.frontmatter.description}</div> */}
-                                        {/* <div className='flex'>
-                                            {
-                                                post.node.frontmatter.tags.filter(tag => post.node.frontmatter.tags !== null)
-                                                .map(tag => {
-                                                    return (
-                                                        <div className='pr-4 pt-1 hidden md:block'>
-                                                            <div className='text-xs rounded-full py-1 px-3 border cursor-default border-trueGray-600 hover:text-orange-500'>{tag}</div>
-                                                        </div>
-                                                    );
-                                                })
-                                            }
-                                        </div> */}
-                                        <div className='sm:text-sm md:text-base text-white font-bold sm:py-px md:py-4'>
+                                        <div className='sm:text-sm md:text-base text-white font-bold pt-3'>
                                             By {post.node.frontmatter.author_info.author_name} | {post.node.frontmatter.date}
                                         </div>
                                     </div>
-                                    <div className='hidden lg:block'>
-                                        <div className='pt-7 md:pt-10 lg:pt-14'></div>
+                                    <div className='hidden lg:block justify-center items-center col-span-1'>
                                         <Link to={post.node.fields.slug} key={post.node.id}>
                                             <div className='flex justify-end items-center'>
                                                 <GatsbyImage image={img} alt={post.node.frontmatter.title} className='rounded' />
-                                                <div className='pr-7'></div>
                                             </div>
                                         </Link>
                                     </div>
-                                </div>
                             </div>
                         );
                     })
@@ -55,8 +38,8 @@ function BlogHomeList(props) {
     }
 
     return (
-        <div className='text-white py-8 md:py-12 lg:py-15'>
-            <ul>{renderBlogPostList()}</ul>
+        <div className='text-white py-8 md:py-12 lg:py-15 max-w-4xl flex flex-col'>
+            <div>{renderBlogPostList()}</div>
         </div>
     )
 }
