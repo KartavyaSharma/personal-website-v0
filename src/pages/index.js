49937fca72cc5f36e88f1edf744d9eb70c873c0e
KartavyaSharma <Kartavya@outlook.com>
2021-04-23 13:08:44 +0100

    Vercel test

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ import Projects from "../components/Projects"
 import Blog from "../components/Blog"
 import Footer from "../components/Footer"
 
+//Test
+
 const mobileHeaders = "py-12 md:py-16 lg:py-24 2xl:py-48 px-6 md:px-8 lg:px-12 xl:px-20 2xl:px-36 max-w-screen-2xl w-full mx-auto font-mono";
 const sectionHeaderNum = "text-lg sm:text-xl md:text-2xl lg:text-4xl font-semibold font-mono bg-clip-text text-white";
 const sectionHeaderTitle = "text-5xl md:text-5xl lg:text-8xl xl:text-10xl font-bold font-mono text-orange-500";
