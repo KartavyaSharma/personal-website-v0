c77dfb673211a8993cc0a71b450ad60fdd1f666c
KartavyaSharma <Kartavya@outlook.com>
2021-04-29 09:12:46 +0100

    sytle: Project page structure added + added logo assets

--- src/templates/ProjectPage.js
+++ src/templates/ProjectPage.js
@@ -11,7 +11,7 @@ export default function ProjectPage(props) {
     return (
         <div className='bg-trueGray-900'>
             <Header />
-            <div className="pt-14 lg:pt-16 lg:pb-24 px-6 md:px-8 lg:px-12 xl:px-20 2xl:px-36 mb-16 lg:mb-4 max-w-screen-2xl w-full mx-auto flex" data-sal="fade" data-sal-easing="ease" data-sal-duration="1500">
+            <div className="lg:pb-24 px-6 md:px-8 lg:px-12 xl:px-20 2xl:px-36 mb-16 lg:mb-4 max-w-screen-2xl w-full mx-auto flex" data-sal="fade" data-sal-easing="ease" data-sal-duration="1500">
                 <div className='max-w-4xl flex flex-col justify-center'>
                     <div className='grid grid-rows-1 grid-cols-5 w-full items-center'>
                         <div className='text-5xl md:text-6xl lg:text-7xl text-white font-bold font-mono col-span-4'>{queryData.name}</div>
@@ -19,10 +19,10 @@ export default function ProjectPage(props) {
                             <GatsbyImage image={img} alt={queryData.alt} />
                         </div>
                     </div>                    
-                    <div className='text-3xl text-white font-mono font-semibold mt-10'>Project details</div>
+                    <div className='text-3xl text-white text-opacity-80 font-mono font-semibold mt-10'>Project details</div>
                     <p className='text-white font-blogBody pt-5'>{queryData.about}</p>
-                    <div className='text-3xl text-white font-mono font-semibold mt-5'>Technology stack and API usage</div>
-                    <div className='text-3xl text-white font-mono font-semibold mt-5'>My experience</div>
+                    <div className='text-3xl text-white text-opacity-80 font-mono font-semibold mt-5'>Technology stack and API usage</div>
+                    <div className='text-3xl text-white text-opacity-80 font-mono font-semibold mt-5'>My experience</div>
                 </div>
             </div>
             <Footer isPage={true} />
@@ -50,8 +50,8 @@ export const projectQuery = graphql`
                         formats: AUTO
                         transformOptions: {fit: COVER}
                         layout: CONSTRAINED
-                        width: 128
-                        height: 128
+                        width: 256
+                        height: 256
                         quality: 100
                     )
                 }
