ead70cae1d9a545628818bc82957095943d411e5
KartavyaSharma <Kartavya@outlook.com>
2021-05-12 14:29:11 +0100

    fix: Large code blocks on mobile should not overflow now

--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -27,7 +27,7 @@ class Content extends React.Component {
     render() {
         return (
             <div>
-                <div className='font-blogBody pb-10' dangerouslySetInnerHTML={{ __html: this.props.hData.markdownRemark.html }}></div>
+                <div className='font-blogBody pb-10 max-w-full' dangerouslySetInnerHTML={{ __html: this.props.hData.markdownRemark.html }}></div>
                 <div className='text-3xl font-mono text-white font-semibold pb-2'>Comments</div>
                 <hr className='border-orange-500 hidden md:block md:min-w-keepWmd 2xl:min-w-keepWlg'/>
                 <div id='inject-comments' className='md:pt-8'></div>
@@ -65,7 +65,7 @@ function BlogPost({ data }) {
                 where={`https://www.kartavyas.com${data.markdownRemark.fields.slug}`}
             />
             <div className="pt-10 2xl:pt-16 lg:pb-24 px-6 md:px-8 lg:px-12 xl:px-20 2xl:px-36 mb-16 lg:mb-4 max-w-screen-2xl w-full mx-auto flex">
-                <div className='max-w-3xl 2xl:max-w-4xl flex flex-col justify-center'>
+                <div className='max-w-full lg:max-w-3xl 2xl:max-w-4xl flex flex-col justify-center'>
                     <div className='text-4xl lg:text-6xl 2xl:text-7xl text-white font-bold font-mono'>{data.markdownRemark.frontmatter.title}</div>
                     <div className='text-lg lg:text-xl text-trueGray-500 italic mt-4 font-mono'>{data.markdownRemark.frontmatter.description}</div>
                     <div className='flex items-center'>
