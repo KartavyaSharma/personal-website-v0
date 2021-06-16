58f451c30d457d329c526a1e41e5dec00eb53880
KartavyaSharma <Kartavya@outlook.com>
2021-06-16 12:25:20 +0530

    fix: removed project card-based links

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,17 +53,15 @@ const config = {
         {
             resolve: `gatsby-plugin-scroll-reveal`,
             options: {
-                threshold: 0.1, // Percentage of an element's area that needs to be visible to launch animation
-                once: true, // Defines if animation needs to be launched once
-                disable: false, // Flag for disabling animations
-
-                // Advanced Options
-                selector: `[data-sal]`, // Selector of the elements to be animated
-                animateClassName: `sal-animate`, // Class name which triggers animation
-                disabledClassName: `sal-disabled`, // Class name which defines the disabled state
-                rootMargin: `0% 50%`, // Corresponds to root's bounding box margin
-                enterEventName: `sal:in`, // Enter event name
-                exitEventName: `sal:out`, // Exit event name
+                threshold: 0.1,
+                once: true,
+                disable: false,
+                selector: `[data-sal]`,
+                animateClassName: `sal-animate`,
+                disabledClassName: `sal-disabled`,
+                rootMargin: `0% 50%`,
+                enterEventName: `sal:in`,
+                exitEventName: `sal:out`,
             }
         },
         `gatsby-plugin-postcss`,
