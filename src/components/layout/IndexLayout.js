c7f90337d1eb42584a695654a902e98eef9489fb
Kartavya Sharma <Sharmakartavya06@gmail.com>
2021-07-04 06:29:18 +0000

    style: updated indexLayout.js anims

--- src/components/layout/IndexLayout.js
+++ src/components/layout/IndexLayout.js
@@ -9,11 +9,25 @@ export default function IndexLayout({ children, customClass, ident, idx, name })
         <div 
             className={classNames('py-12 md:py-16 lg:py-18 2xl:py-48 px-6 md:px-8 lg:px-12 xl:px-20 2xl:px-36 max-w-screen-2xl w-full mx-auto '+ customClass)} 
             id={ident}>
-            <div data-sal="slide-right" data-sal-easing="ease" data-sal-duration="1000">
-                <div className='text-lg sm:text-xl md:text-2xl lg:text-4xl font-semibold font-mono bg-clip-text text-highlight'>{idx}</div>
+            <div>
+                <div 
+                    className='text-lg sm:text-xl md:text-2xl lg:text-4xl font-semibold font-mono bg-clip-text text-highlight' 
+                    data-sal="slide-right"
+                    data-sal-easing="ease" 
+                    data-sal-duration="1000"
+                >
+                    {idx}
+                </div>
                 <div className='flex flex-row items-center'>
-                    <div className='text-5xl md:text-5xl lg:text-8xl font-bold font-mono text-white mr-5 flex-nowrap min-w-max'>{name}</div>
-                    <hr className='border border-border mt-5 w-full' data-sal="slide-right" data-sal-easing="ease" data-sal-duration="300"/>
+                    <div 
+                        className='text-5xl md:text-5xl lg:text-8xl font-bold font-mono text-white mr-5 flex-nowrap min-w-max'
+                        data-sal="slide-right"
+                        data-sal-easing="ease" 
+                        data-sal-duration="1000"
+                    >
+                        {name}
+                    </div>
+                    <hr className='border border-border mt-5 w-full' data-sal="slide-left" data-sal-easing="ease" data-sal-duration="800"/>
                 </div>
             </div>
             <div className='mt-10 md:mt-16 lg:mt-18'>
