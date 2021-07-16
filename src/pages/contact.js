f0bc532314f6cb5e0e1fec24689a53bf77c582af
Kartavya Sharma <Sharmakartavya06@gmail.com>
2021-07-16 09:40:37 +0000

    Updated autolink headers

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Header from "../components/Header"
 import { FirebaseProvider } from '../context/FirebaseContext'
 import Auth from '../utils/Auth/Auth'
