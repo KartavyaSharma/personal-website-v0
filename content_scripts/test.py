09c339548b536805cdc45c28783f0a02dfb57c27
Kartavya Sharma <Sharmakartavya06@gmail.com>
2021-08-01 19:49:39 +0000

    refactor: removed test files

--- content_scripts/test.py
+++
@@ -1,9 +0,0 @@
-from sqlitedict import SqliteDict
-
-mydict = SqliteDict('key_value_store.sqlite', autocommit=True)
-mydict['some_key']='Hello'
-print(mydict['some_key'])
-mydict.close()
-
-mydict = SqliteDict('key_value_store.sqlite')
-print(mydict['some_key'])
