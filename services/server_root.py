4931b262e9155cbb9655c9024c1ade81f6d2c7e7
Kartavya Sharma <kartavya@outlook.com>
2021-11-26 11:57:28 +0530

    added backend handlers

---
+++ services/server_root.py
@@ -0,0 +1,19 @@
+from flask import render_template
+import connexion
+from flask_cors import CORS
+
+app = connexion.App(__name__, specification_dir='./spec', options={'swagger_ui': False})
+
+app.add_api('specification.yml')
+CORS(app.app)
+
+@app.route('/')
+def home():
+    """
+    Static kartavys-backend page, responds to the "/" route
+    :return:    Static welcome template 'home.html'
+    """
+    return "Hello, world!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
